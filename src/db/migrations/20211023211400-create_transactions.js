71eed666a62297c8bf9e5cc7ecc9909ef7615f59
Raverside <r4verside@gmail.com>
2021-10-25 13:43:06 +0300

    Transactions

--- src/db/migrations/20211023211400-create_transactions.js
+++ src/db/migrations/20211023211400-create_transactions.js
@@ -2,20 +2,40 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('transactions', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        references: { model: 'users', key: 'id' }
+      },
+      activated_by: {
+        type: Sequelize.UUID,
+        references: { model: 'users', key: 'id' }
+      },
+      membership_id: {
+        type: Sequelize.UUID,
+        references: { model: 'memberships', key: 'id' }
+      },
+      amount: Sequelize.INTEGER,
+      currency: Sequelize.STRING,
+      method: Sequelize.STRING,
+      notes: Sequelize.TEXT,
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('transactions');
   }
 };
