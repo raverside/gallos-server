e6182f9de0baf6402a74b3a43341ad3837780ec3
Raverside <r4verside@gmail.com>
2021-11-04 22:37:40 +0200

    updated the db config

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "postgres",
-    "password": null,
-    "database": "gallos",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
