8421f49dd1f56eca780356b793b1509df9e166aa
Raverside <r4verside@gmail.com>
2021-11-04 22:48:57 +0200

    updated the db config

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,6 +1,8 @@
+require('dotenv').config({path:__dirname+'/../../../.env'});
+
 module.exports = {
   "development": {
-    "username": "postgres",
+    "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD || null,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
