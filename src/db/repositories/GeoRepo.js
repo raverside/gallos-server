7361e6ddce39c71ea38ef0d098a9ea7f4c5a7de2
Raverside <r4verside@gmail.com>
2021-11-07 21:16:17 +0200

    geo sorted alphabetically

--- src/db/repositories/GeoRepo.js
+++ src/db/repositories/GeoRepo.js
@@ -39,11 +39,11 @@ class GeoRepo {
     }
 
     async getAllCountries() {
-        return await geo_countries.findAll({order: [['name', 'DESC']]});
+        return await geo_countries.findAll({order: [['name', 'ASC']]});
     }
 
     async getCountriesWithStadium() {
-        return await geo_countries.findAll({include: {model: stadiums, required: true}, order: [['name', 'DESC']]});
+        return await geo_countries.findAll({include: {model: stadiums, required: true}, order: [['name', 'ASC']]});
     }
 
     async getStateById(id) {
@@ -51,7 +51,7 @@ class GeoRepo {
     }
 
     async getStatesByCountry(country_id) {
-        return await geo_states.findAll({where: {country_id}, order: [['name', 'DESC']]});
+        return await geo_states.findAll({where: {country_id}, order: [['name', 'ASC']]});
     }
 
     async getCityById(id) {
@@ -59,7 +59,7 @@ class GeoRepo {
     }
 
     async getCitiesByState(state_id) {
-        return await geo_cities.findAll({where: {state_id}, order: [['name', 'DESC']]});
+        return await geo_cities.findAll({where: {state_id}, order: [['name', 'ASC']]});
     }
 
 }
