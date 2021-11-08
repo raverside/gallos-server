48a06f449a41181d8ea2a887ef0a8b1584a3ece8
Raverside <r4verside@gmail.com>
2021-11-09 01:05:14 +0200

    updated digital_ids generation logic

--- src/db/repositories/TeamOwnerRepo.js
+++ src/db/repositories/TeamOwnerRepo.js
@@ -69,7 +69,7 @@ class TeamOwnerRepo {
             await team_owners.update(teamOwner, {where: {id: teamOwner.id}});
             return teamOwner;
         } else {
-            const digital_id = (Math.random() * (999999999 - 100000000 + 1) ) << 0;
+            const digital_id = Math.floor(Math.random() * (999999999  - 100000000 + 1)) + 100000000;
             try {
                 return await team_owners.create({...teamOwner, digital_id});
             } catch (err) {
@@ -106,7 +106,7 @@ class TeamOwnerRepo {
     }
 
     async createTeam(team_owner_id, name) {
-        const digital_id = (Math.random() * (999999999 - 100000000 + 1) ) << 0;
+        const digital_id = Math.floor(Math.random() * (999999999  - 100000000 + 1)) + 100000000;
         try {
             const response = await teams.create({team_owner_id, name, digital_id})
             if (response) this.preventLoop = 0;
